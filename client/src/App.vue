4b1b305b0884e832ea3b9cc7e5bbb584053a3f3b
Anh Cao <ctpanh03@gmail.com>
2023-07-03 16:47:54 +0700

    base frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { useAuthStore } from './stores/auth';
+
+const token = localStorage.getItem("token");
 </script>
 
 <template>
@@ -8,10 +10,12 @@ import { useAuthStore } from './stores/auth';
     <div class="max-w-7xl mx-auto">
       <nav class="p-2">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/notes">Notes</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
         <RouterLink to="/register">Register</RouterLink>
-        <button @click="useAuthStore().handleLogout">Logout</button>
+        <div v-if="token"> 
+          <RouterLink to="/notes">Notes</RouterLink>
+          <button @click="useAuthStore().handleLogout">Logout</button>
+        </div>
       </nav>
     </div>
   </header>
